affc9db582b29f8d72d0d66a4e27218761ec07b4
StellarSt0rm <StellarSt0rm@proton.me>
2025-01-03 18:51:48 +0100

    Some improvements, a bit of inline docs, test for modifiying CSS properties

--- src/desktop.rs
+++ src/desktop.rs
@@ -1,41 +1,45 @@
-use web_sys::{Document, Element};
+use wasm_bindgen::JsCast;
+use web_sys::{Document, Element, HtmlElement};
 
 pub struct Desktop {
     container: Element,
     document: Document,
     window_pool: Vec<Window>,
-    pub active_window: Option<u32>,
 }
 
+/// The 'layer' field goes from 0 to n, 0 being the topmost.
 pub struct Window {
     pub layer: usize,
+    pub id: usize,
     pub name: String,
     pub content: String,
-    pub icon_url: String,
 }
 
 impl Desktop {
     pub fn new(container: Element) -> Desktop {
+        //! Create a new Desktop, a container must be given.
         let document = web_sys::window().unwrap().document().unwrap();
 
         Desktop {
             container,
             document,
             window_pool: Vec::new(),
-            active_window: None,
         }
     }
 
     pub fn new_window(&mut self, name: String, content: String, icon_url: String) -> &Window {
+        //! Create a new window, and add it to the container
         let window = Window {
-            layer: 0,
+            layer: 0, // Layer set to 0 for new windows.
+            id: self.window_pool.len() + 1,
             name,
             content,
-            icon_url,
         };
 
-        let text = self.document.create_element("p").unwrap();
+        let text: HtmlElement = self.document.create_element("p").unwrap().unchecked_into();
         text.set_text_content(Some(&window.name));
+        text.set_id(&window.id.to_string());
+        text.style().set_property("z-index", "0").unwrap();
 
         self.container.append_child(&text).unwrap();
 
