c6f862e314d52b156a808f4bcc0073eb11026b27
StellarSt0rm <StellarSt0rm@proton.me>
2025-05-30 20:02:53 +0200

    Make the parser return &str, makes some thing easier

--- src/sdo_parser.rs
+++ src/sdo_parser.rs
@@ -3,9 +3,9 @@ use include_dir::File;
 // Inverted until I figure out a better way to do the parsing
 const SDO_SECTIONS: [&str; 3] = ["Content:", "Icon:", "Title:"];
 
-pub fn parse<'a>(files: impl Iterator<Item = &'a File<'a>>) -> Vec<Vec<String>> {
+pub fn parse<'a>(files: impl Iterator<Item = &'a File<'a>>) -> Vec<Vec<&'a str>> {
     let files = files.collect::<Vec<_>>();
-    let mut parsed_files: Vec<Vec<String>> = Vec::with_capacity(files.len());
+    let mut parsed_files: Vec<Vec<&str>> = Vec::with_capacity(files.len());
 
     for file in files {
         // Skip non .sdo files
@@ -14,12 +14,12 @@ pub fn parse<'a>(files: impl Iterator<Item = &'a File<'a>>) -> Vec<Vec<String>> 
         }
 
         let mut content = file.contents_utf8().unwrap();
-        let mut parsed_sections: Vec<String> = Vec::with_capacity(SDO_SECTIONS.len());
+        let mut parsed_sections: Vec<&str> = Vec::with_capacity(SDO_SECTIONS.len());
 
         for section in SDO_SECTIONS {
             let (remaining, parsed_section) = content.split_once(section).unwrap();
 
-            parsed_sections.push(parsed_section.trim().to_string());
+            parsed_sections.push(parsed_section.trim());
             content = remaining;
         }
 
